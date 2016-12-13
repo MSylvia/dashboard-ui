1a9808e0efa4a5b7612e967b9422816fb4f7d37b
Jignesh Chaudhari <jignesh2481991@gmail.com>
2016-12-13 08:54:49 +0530

    fixed linting error

--- app/reducers/userList.js
+++ app/reducers/userList.js
@@ -3,13 +3,14 @@
  */
 export default function (state = {}, action) {
     switch (action.type) {
-        case  'RECEIVE_USERS':
+        case 'RECEIVE_USERS': {
             console.log('Inside RECEIVE_USERS reducer');
             let userObj = {};
-            for(let i = 0 ; i < action.payload.length; i++ ){
+            for (let i = 0; i < action.payload.length; i++) {
                 userObj[action.payload[i]._id] = action.payload[i];
             }
-            return {...state,...userObj};
+            return {...state, ...userObj};
+        }
 
         default:
             return state;
