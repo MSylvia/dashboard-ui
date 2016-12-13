453926fffe28b7125e9dc28ed96ff7365994590a
Jignesh Chaudhari <jignesh2481991@gmail.com>
2016-12-13 15:57:30 +0530

    change for deployment

--- app/config.js
+++ app/config.js
@@ -2,8 +2,8 @@
  * Created by Darkstar on 12/4/2016.
  */
 module.exports = {
-    dashboardAPI : 'http://localhost:3000',
+    dashboardAPI : 'https://service.cloudboost.io',
     analyticsAPI : '',
-    accountsURL : 'http://localhost:1447',
-    accountsAPI : 'http://localhost:3000'
+    accountsURL : 'https://accounts.cloudboost.io',
+    accountsAPI : 'https://service.cloudboost.io'
 };
