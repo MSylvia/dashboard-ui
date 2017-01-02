390667f0c8004e54553c14713977b34214e801ae
Jignesh Chaudhari <jignesh2481991@gmail.com>
2017-01-02 12:09:13 +0530

    set up config based on production or dev environment

--- app/config.js
+++ app/config.js
@@ -1,9 +1,23 @@
 /**
  * Created by Darkstar on 12/4/2016.
  */
-module.exports = {
-    dashboardAPI : 'https://service.cloudboost.io',
-    analyticsAPI : '',
-    accountsURL : 'https://accounts.cloudboost.io',
-    accountsAPI : 'https://service.cloudboost.io'
+let globals = () => {
+    console.log("value of environment variable NODE_ENV");
+    console.log(process.env["NODE_ENV"]);
+    if (process.env["NODE_ENV"] === "production") {
+        return {
+            dashboardAPI: 'https://service.cloudboost.io',
+            analyticsAPI: '',
+            accountsURL: 'https://accounts.cloudboost.io',
+            accountsAPI: 'https://service.cloudboost.io'
+        };
+    }
+    return {
+        dashboardAPI: 'http://localhost:3000',
+        analyticsAPI: '',
+        accountsURL: 'http://localhost:1447',
+        accountsAPI: 'http://localhost:3000'
+    };
 };
+
+module.exports = globals();
